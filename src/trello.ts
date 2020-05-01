24a6f14f7ff4ec94f28291a6ba224a1a7a44ed72
Aasmund Grammeltvedt <asgramme@microsoft.com>
2020-05-01 15:48:36 +0200

    Init

---
+++ src/trello.ts
@@ -0,0 +1,221 @@
+const root = "https://api.trello.com";
+
+interface Board {
+  id: string;
+  desc: string;
+  descData: string;
+  closed: boolean;
+  idOrganization: string;
+  pinned: boolean;
+  url: string;
+  shortUrl: string;
+  prefs: {
+    permissionLevel: string;
+    hideVotes: boolean;
+    voting: "disabled";
+    comments: string;
+    selfJoin: boolean;
+    cardCovers: boolean;
+    isTemplate: boolean;
+    cardAging: "pirate";
+    calendarFeedEnabled: boolean;
+    background: string;
+    backgroundImage: string;
+    backgroundImageScaled: string;
+  };
+  labelNames: {
+    green: string;
+    yellow: string;
+    orange: string;
+    red: string;
+    purple: string;
+    blue: string;
+    sky: string;
+    lime: string;
+    pink: string;
+    black: string;
+  };
+  limits: {
+    attachments: {
+      perBoard: {
+        status: "ok";
+        disableAt: number;
+        warnAt: number;
+      };
+    };
+  };
+  starred: boolean;
+  memberships: string;
+  shortLink: string;
+  subscribed: boolean;
+  powerUps: string;
+  dateLastActivity: string;
+  dateLastView: string;
+  idTags: string;
+  datePluginDisable: string;
+  creationMethod: string;
+  ixUpdate: number;
+  templateGallery: string;
+  enterpriseOwned: boolean;
+}
+
+interface Card {
+  id: string;
+  address: string;
+  badges: {
+    attachmentsByType: {
+      trello: {
+        board: number;
+        card: number;
+      };
+    };
+    location: boolean;
+    votes: number;
+    viewingMemberVoted: boolean;
+    subscribed: boolean;
+    fogbugz: string;
+    checkItems: number;
+    checkItemsChecked: number;
+    comments: number;
+    attachments: number;
+    description: boolean;
+    due: string;
+    dueComplete: boolean;
+  };
+  checkItemStates: [string];
+  closed: boolean;
+  coordinates: string;
+  creationMethod: string;
+  dateLastActivity: string;
+  desc: string;
+  descData: {
+    emoji: {};
+  };
+  due: string;
+  dueReminder: string;
+  email: string;
+  idBoard: string;
+  idChecklists: string[];
+  idLabels: string[];
+  idList: string;
+  idMembers: [string];
+  idMembersVoted: [string];
+  idShort: number;
+  labels: {
+    id: string;
+    idBoard: string;
+    name: string;
+    color: string;
+  }[];
+  limits: {
+    attachments: {
+      perBoard: {
+        status: string;
+        disableAt: number;
+        warnAt: number;
+      };
+    };
+  };
+  locationName: string;
+  manualCoverAttachment: boolean;
+  name: string;
+  pos: number;
+  shortLink: string;
+  shortUrl: string;
+  subscribed: boolean;
+  url: string;
+  cover: {
+    color: string;
+    idUploadedBackground: boolean;
+    size: string;
+    brightness: string;
+    isTemplate: boolean;
+  };
+}
+
+export interface ChecklistItem {
+  idChecklist: string;
+  state: "complete";
+  idMember: null;
+  id: string;
+  name: string;
+  nameData: null;
+  pos: number;
+  due: null;
+}
+
+export interface Checklist {
+  id: string;
+  name: string;
+  idCard: string;
+  pos: number;
+  idBoard: string;
+  checkItems: ChecklistItem[];
+}
+
+export type TrelloClient = ReturnType<typeof trello>;
+export interface RequestFactory {
+  (
+    method: "get" | "delete" | "post" | "put",
+    url: string,
+    params: {},
+    data?: unknown
+  ): Promise<{}>;
+}
+
+export const trello = (opts: {
+  key: string;
+  token: string;
+  makeRequest: RequestFactory;
+}) => {
+  const { key, token, makeRequest } = opts;
+
+  return {
+    getBoard: (id: string) =>
+      makeRequest("get", `${root}/1/boards/${id}`, { key, token }) as Promise<
+        Board
+      >,
+    getBoardChecklists: (id: string) =>
+      makeRequest("get", `${root}/1/boards/${id}/checklists`, {
+        key,
+        token,
+        fields: "all",
+      }) as Promise<Board>,
+    getBoardCards: (id: string) =>
+      makeRequest("get", `${root}/1/boards/${id}/cards`, {
+        key,
+        token,
+      }) as Promise<Card[]>,
+    getCard: (id: string) =>
+      makeRequest("get", `${root}/1/cards/${id}`, { key, token }) as Promise<
+        Card
+      >,
+
+    getChecklist: (id: string) =>
+      makeRequest("get", `${root}/1/checklists/${id}`, {
+        key,
+        token,
+      }) as Promise<Checklist>,
+    updateChecklist: (id: string, data: Checklist) =>
+      makeRequest("put", `${root}/1/checklists/${id}`, { key, token }, data),
+
+    updateChecklistItem: (
+      idCard: string,
+      idChecklist: string,
+      idCheckItem: string,
+      data: ChecklistItem
+    ) =>
+      makeRequest(
+        "put",
+        `${root}/1/cards/${idCard}/checklist/${idChecklist}/checkItem/${idCheckItem}`,
+        { key, token },
+        data
+      ),
+
+    removeLabel: (idCard: string, idLabel: string) =>
+      makeRequest("delete", `${root}/1/cards/${idCard}/idLabels/${idLabel}`, {
+        key,
+        token,
+      }),
+  };
+};
---
+++ src/trelloClient.ts
@@ -0,0 +1,174 @@
+import * as fs from "fs";
+import Axios from "axios";
+import EloRank from "elo-rank";
+import debug from "debug";
+import "axios-debug-log";
+
+const logger = debug("shopr:trelloClient");
+
+import { trello, Checklist, TrelloClient } from "./trello";
+
+interface Prefs {
+  token: string;
+  key: string;
+  board: string;
+  trainLabel: string;
+  orderLabel: string;
+}
+
+interface Scores {
+  [key: string]: number | undefined;
+}
+
+const DEFAULT_SCORE = 1000;
+
+// Get trello client
+function client(prefs: Prefs): TrelloClient {
+  return trello({
+    key: prefs.key,
+    token: prefs.token,
+    // fixme force put as application/json and not formencoded
+    makeRequest: async (method, url, params, data) => {
+      try {
+        return (await Axios.request({ method, url, params, data })).data;
+      } catch (err) {
+        logger(err?.response?.data);
+        throw err;
+      }
+    },
+  });
+}
+
+// Get list of checklists to train on
+async function trainSet(
+  client: TrelloClient,
+  prefs: Prefs
+): Promise<Checklist[]> {
+  const cards = await client.getBoardCards(prefs.board);
+  const trainCheklistIds = cards.reduce<string[]>(
+    (acc, card) =>
+      card.labels.find((l) => l.name === prefs.trainLabel)
+        ? [...acc, ...card.idChecklists]
+        : acc,
+    []
+  );
+
+  let trainChecklists: Checklist[] = [];
+  for (const t of trainCheklistIds) {
+    trainChecklists = [...trainChecklists, await client.getChecklist(t)];
+  }
+
+  return trainChecklists;
+}
+
+// Remove training label from cards containing given checklists
+async function resetLabel(
+  client: TrelloClient,
+  labelName: string,
+  idCards: string[]
+): Promise<void> {
+  for (const idCard of idCards) {
+    const card = await client.getCard(idCard);
+    await Promise.all(
+      card.labels
+        .reduce<string[]>(
+          (acc, label) => (label.name === labelName ? [...acc, label.id] : acc),
+          []
+        )
+        .map((idLabel) => client.removeLabel(card.id, idLabel))
+    );
+  }
+}
+
+// Order list according to score
+async function orderList(
+  client: TrelloClient,
+  scores: Scores,
+  prefs: Prefs
+): Promise<void> {
+  const cards = await client.getBoardCards(prefs.board);
+  for (const card of cards) {
+    if (!card.labels.find((l) => l.name === prefs.orderLabel)) {
+      continue;
+    }
+
+    logger(`Ordering ${card.name}`);
+    for (const idChecklist of card.idChecklists) {
+      const checklist = await client.getChecklist(idChecklist);
+
+      for (const checklistItem of checklist.checkItems) {
+        logger(checklistItem);
+        // Avoid negative pos values
+        const pos = (scores[checklistItem.name] ?? DEFAULT_SCORE) + 100000;
+        if (checklistItem.pos != pos) {
+          await client.updateChecklistItem(
+            card.id,
+            idChecklist,
+            checklistItem.id,
+            { ...checklistItem, pos }
+          );
+        }
+      }
+    }
+    logger(`Ordering ${card.name} done`);
+
+    await resetLabel(client, prefs.orderLabel, [card.id]);
+  }
+}
+
+function train(list: Checklist, oldScores: Readonly<Scores>): Scores {
+  logger(`Training on ${list.checkItems.length} items`);
+  var elo = new EloRank();
+
+  const scores: Scores = { ...oldScores };
+
+  list.checkItems.forEach((current) => {
+    const currentScore = scores[current.name] ?? DEFAULT_SCORE;
+    list.checkItems.forEach((compare) => {
+      // Same object
+      if (current.pos === compare.pos) {
+        return;
+      }
+
+      // Lower score means earlier in the sequence, to match Trello's
+      // ordering
+      const compareScore = scores[compare.name] ?? DEFAULT_SCORE;
+      scores[current.name] = elo.updateRating(
+        elo.getExpected(currentScore, compareScore),
+        current.pos < compare.pos ? 0 : 1,
+        currentScore
+      );
+      scores[compare.name] = elo.updateRating(
+        elo.getExpected(compareScore, currentScore),
+        compare.pos < current.pos ? 0 : 1,
+        compareScore
+      );
+    });
+  });
+
+  return scores;
+}
+
+async function main() {
+  const prefs = JSON.parse(fs.readFileSync(".trello.json", "utf-8")) as Prefs;
+  const c = client(prefs);
+  const checkLists = await trainSet(c, prefs);
+
+  const scores = fs.existsSync("scores.json")
+    ? JSON.parse(fs.readFileSync("scores.json", "utf-8"))
+    : {};
+  const newScores = checkLists.reduce<Scores>(
+    (scores, checkList) => train(checkList, scores),
+    scores
+  );
+  fs.writeFileSync("scores.json", JSON.stringify(newScores));
+
+  await resetLabel(
+    c,
+    prefs.trainLabel,
+    checkLists.map((c) => c.idCard)
+  );
+  await orderList(c, scores, prefs);
+}
+
+main();
